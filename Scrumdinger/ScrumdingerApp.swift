41fb7de68f646f8b40297db675973dce18e2dc74
Michael Kennedy <michaelkennedy_228@hotmail.com>
2023-11-02 14:42:13 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Michael Kennedy on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
