70e576afbac973b2bb9a33a0061bc67f50b9e7e1
Michael Kennedy <michaelkennedy_228@hotmail.com>
2023-11-03 00:36:07 -0300

    First Commit - Start Creating a navigation hierarchy

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Michael Kennedy on 2023-11-02.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack{
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
