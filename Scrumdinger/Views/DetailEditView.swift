368ed08e1db4322a35bfeac0203ee03ec3510f17
Michael Kennedy <michaelkennedy_228@hotmail.com>
2023-11-04 03:42:17 -0300

    Create a new SwiftUI View file named DetailEditView.swift.

---
+++ Scrumdinger/Views/DetailEditView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Michael Kennedy on 2023-11-04.
+//
+
+import SwiftUI
+
+
+struct DetailEditView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
