62555bf3f0c39a697b1b7f1cf646560ded99a0fe
Michael Kennedy <michaelkennedy_228@hotmail.com>
2023-11-04 02:25:37 -0300

    Wrap DetailView in a NavigationStack to preview navigation elements on the canvas. In ScrumsView.swift, update the NavigationLink to set DetailView as the new destination.

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -7,12 +7,21 @@
 
 import SwiftUI
 
+
 struct DetailView: View {
+    let scrum: DailyScrum
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
 }
 
-#Preview {
-    DetailView()
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
 }
