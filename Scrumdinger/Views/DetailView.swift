9b88a7a389b35a17b0df637ce285a87b8dace470
Michael Kennedy <michaelkennedy_228@hotmail.com>
2023-11-04 02:45:31 -0300

    In DetailView.swift, add a Spacer(), and display the theme name in a Text view. Add accessibilityElement(children:) to the HStack to combine the Label and Text elements in VoiceOver.

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -26,7 +26,10 @@ struct DetailView: View {
                 .accessibilityElement(children: .combine)
                 HStack {
                     Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
                 }
+                .accessibilityElement(children: .combine)
             }
         }
     }
