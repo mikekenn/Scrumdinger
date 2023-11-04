1023718ce141e12fa6799c5de93094d2e91c935b
Michael Kennedy <michaelkennedy_228@hotmail.com>
2023-11-04 02:24:58 -0300

    Add a scrum constant. In the preview provider, initialize DetailView with a sample scrum. Wrap DetailView in a NavigationStack to preview navigation elements on the canvas.

---
+++ Scrumdinger/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Michael Kennedy on 2023-11-04.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
